996cc8654d573a1e87bdbbae1c3b4d62f3b474af
wellecks <wellecks@gmail.com>
2013-04-07 00:01:01 -0400

    added stuff

--- app/models/redirect_mapping.rb
+++ app/models/redirect_mapping.rb
@@ -1,12 +1,6 @@
 class RedirectMapping
-  # image_tag_uri
-  # char
-
   def image_tag_uri
-    if @image_tag_uri.nil?
-      @image_tag_uri = @r[:image_tag_uri]
-    end
-    return @image_tag_uri
+    @image_tag_uri
   end
 
   def url
@@ -14,50 +8,36 @@ class RedirectMapping
   end
 
   def char
-    if @char.nil?
-      @char = @r[:char]
-    end
-    return @char
+    return @r[image_tag_uri]
   end
 
   def char=
-    @r[:char].set(value)
+    @r[image_tag_uri].set(value)
   end
 
   def initialize
     @r = Nest.new(self.class.to_s.underscore)
-    self.genreate_image_tag_uri
+    @image_tag_uri = self.generate_image_tag_uri
   end
 
-  def genreate_image_tag_uri
-    self.image_tag_uri = loop do
+  def generate_image_tag_uri
+    tag_uri = loop do
       random_token = SecureRandom.urlsafe_base64
       break random_token unless @r[random_token].present?
     end
-
-    @r[@key] = image_key
+    return tag_uri
   end
 
   def self.create(params)
-    raise "cur_char must be a one letter string" unless cur_char.class == String && cur_char.length == 1
-    
+    raise "cur_char must be a one letter string" unless cur_char.class == String && cur_char.length == 1 && cur_char != "nil"
     char = params[:char]
-    # styling = params[:styling] #TODO found with Nokogiri
-
-    image = Image.create(cur_char)
-    image_key = image.key
-    
-    email_pixel = EmailPixel.new
-    email_pixel.write_image_key(image_key)
-
-    return email_pixel
+    self.char = char
   end
 
 private
 
   def image_tag_uri=(value)
-    @r[:image_tag_uri].set(value)
+    @image_tag_uri = value
   end
-
 
 end
